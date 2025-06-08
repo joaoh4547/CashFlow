b9661352be5abbbf2782e870e946577958b62afe
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-06-08 20:53:16 -0300

    Capitulo 1 Parte 2

---
+++ src/CashFlow.Api/Controllers/ExpansesController.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpansesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RegisterExpanseRequest request)
+    {
+        return Created();
+    }
+}
---
+++ src/CashFlow.Communication/Requests/RegisterExpanseRequest.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Communication.Enums;
+
+namespace CashFlow.Communication.Requests;
+public class RegisterExpanseRequest
+{
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public PaymentType Type { get; set; }
+}
