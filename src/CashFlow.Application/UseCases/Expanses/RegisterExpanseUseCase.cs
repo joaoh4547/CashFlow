9583c9c6f843af6ae07189824067e3b29db85756
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-06-08 21:25:24 -0300

    Capitulo 2 Parte 1

--- src/CashFlow.Application/UseCases/Expanses/RegisterExpanseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expanses/RegisterExpanseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expanses;
@@ -6,9 +7,43 @@ public class RegisterExpanseUseCase
 {
     public RegisterExpanseResponse Handle(RegisterExpanseRequest request)
     {
+
+        Validate(request);
+
         return new RegisterExpanseResponse
         {
             Title = request.Title
         };
+    }
+
+
+    private void Validate(RegisterExpanseRequest request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required.");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be grather than zero.");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+
+        if (result > 0)
+        {
+            throw new ArgumentException("Expanses cannot be for future");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.Type);
+
+        if (!paymentTypeIsValid)
+        {
+            throw new ArgumentException("Payment type is not valid.");
+        }
+
     }
 }
